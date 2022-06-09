5e4f559ac32ecd5c3c32427b5101b8d8fcd7290d
HotPinkTangerine <106705653+HotPinkTangerine@users.noreply.github.com>
2022-06-09 16:17:16 -0700

    bacon score

--- main.ts
+++ main.ts
@@ -1,457 +1,57 @@
 controller.up.onEvent(ControllerButtonEvent.Pressed, function () {
-    animation.runImageAnimation(
-    theG,
-    [img`
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . c c . . . . . . . . . . . . 
-        . c 3 6 c c c c c . . . . . . . 
-        . c 6 6 3 3 3 6 6 c . . . . . . 
-        c 3 3 3 3 3 3 6 6 6 c . . . . . 
-        c 3 3 3 3 3 3 6 6 6 6 c . . . . 
-        c 3 3 3 3 3 6 6 6 6 6 c c c . . 
-        c 3 3 3 3 6 6 6 6 6 c 3 3 3 c . 
-        c c 3 3 6 6 6 6 6 c 3 c c c 3 c 
-        c c 6 6 6 6 6 6 c 3 c c c c 6 c 
-        c 3 3 3 3 6 6 c 6 6 c c c c 6 c 
-        c 6 3 3 3 3 6 c 6 c c c c 6 c . 
-        . c 6 3 3 3 6 c c c c c 6 c . . 
-        . . c c c c c c c c c c c . . . 
-        `,img`
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . c c . . . . . . . . . . . 
-        . . c 3 6 c c c c c . . . . . . 
-        . c c 6 3 3 3 3 6 6 c . . . . . 
-        c 6 3 3 3 3 3 6 6 6 6 c . . . . 
-        c 3 3 3 3 3 6 6 6 6 6 c . . . . 
-        c 3 3 3 3 6 6 6 6 6 6 c c c . . 
-        c c 6 6 6 6 6 6 6 6 c 3 3 3 c . 
-        c 6 3 3 3 6 6 6 c c 3 c c c 6 c 
-        c 3 3 3 3 3 6 c c c c c c c c c 
-        . c c c 6 6 c 4 5 5 c c 4 5 5 c 
-        . . . c 6 6 6 c 5 5 5 c 5 5 4 c 
-        . . . . c c c c c c c c c c c . 
-        `,img`
-        . . . . . . . . . . . . . . . . 
-        . . . c c . . . . . . . . . . . 
-        . . c 3 6 c c c c . . . . . . . 
-        . . c 6 3 3 3 3 6 c . . . . . . 
-        . c 3 3 3 3 3 3 6 6 c . . . . . 
-        c 3 3 3 3 3 3 6 6 6 6 c . . . . 
-        c 3 3 3 3 3 c c 6 6 c c c c c . 
-        c c 3 3 3 c 5 5 c c 3 3 c 5 5 b 
-        c c 6 6 6 f f 5 c 3 c c c 5 f f 
-        c 3 3 3 3 f f 5 c c c c c 5 f f 
-        c 3 3 3 3 3 c 5 5 3 c 3 5 5 b . 
-        . c 3 3 3 c c 4 5 5 5 5 4 4 c . 
-        . . b b c 4 5 5 4 4 4 4 5 5 4 c 
-        . c 5 c 4 c 5 5 5 c 4 c 5 5 5 c 
-        . c 5 c 4 c 5 5 5 5 c 5 5 5 5 c 
-        . c c c . . c c c c c c c c c . 
-        `,img`
-        . . . c c . . . . . . . . . . . 
-        . . c 3 6 c 3 c c c c . c c . . 
-        . . c 6 3 3 f f 5 c 6 c 5 f f . 
-        . c 3 3 3 3 f f 5 c 6 c 5 f f . 
-        c 3 3 3 3 3 c 5 5 5 c 5 5 5 b . 
-        c 3 3 3 3 6 c 5 5 5 c 5 5 5 b . 
-        c c 3 3 3 6 3 c 5 5 3 5 5 b b . 
-        c c 6 6 3 3 3 c 5 5 3 5 5 b b . 
-        c 3 3 3 3 c c b 5 5 5 5 5 c c . 
-        c 3 3 3 c 5 5 b 4 5 5 5 4 5 5 c 
-        . c 3 3 c 5 b b 4 4 4 4 b 4 5 b 
-        . . b b c 4 5 5 5 b 4 b 5 5 4 c 
-        . b 5 4 4 c 5 5 5 c 4 c 5 5 5 c 
-        . c 5 c 4 c 5 5 5 c 4 c 5 5 5 c 
-        . c 5 c 4 c 5 5 5 5 c 5 5 5 5 c 
-        . c c c . . c c c c c c c c c . 
-        `,img`
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . c c . . . . . . . . . . . 
-        . . c 3 6 c c c c . . . . . . . 
-        . . c 6 3 3 3 3 6 c . . . . . . 
-        . c 3 3 3 3 3 c c 6 c . c c . . 
-        c 3 3 3 3 3 c 5 5 c 6 c 5 5 c . 
-        c c 3 3 3 6 f f 5 c 6 c 5 f f . 
-        c c 6 6 6 6 f f 5 3 c 3 5 f f . 
-        c 3 3 3 3 3 3 c 5 5 3 5 5 b . . 
-        c 3 3 3 3 c c b 5 5 5 5 5 c c . 
-        . c 3 3 c 5 5 b 4 5 5 5 4 5 5 c 
-        . . b b c 5 b b 4 4 4 4 b 4 5 b 
-        . c 5 c 4 c 5 5 5 c 4 c 5 5 5 c 
-        . c 5 c 4 c 5 5 5 5 c 5 5 5 5 c 
-        . c c c . . c c c c c c c c c . 
-        `,img`
-        . . . c c . . . . . . . . . . . 
-        . . c 3 6 c c c c . . . . . . . 
-        . . c 6 3 3 3 3 6 c . . . . . . 
-        . c 3 3 3 3 3 c c 6 c . c c . . 
-        c 3 3 3 3 3 c 5 5 c 6 c 5 5 b . 
-        c 3 3 3 3 3 f f 5 c 6 c 5 f f . 
-        c c 3 3 3 6 f f 5 c 6 c 5 f f . 
-        c c 6 6 6 6 c 5 5 3 c 3 5 5 b . 
-        c 3 3 3 3 3 3 c 5 5 3 5 5 b . . 
-        c 3 3 3 3 c c b 5 5 5 5 5 c c . 
-        . c 3 3 c 5 5 b 4 5 5 5 4 5 5 c 
-        . . b b c 5 b b 4 4 4 4 b 4 5 b 
-        . b 5 4 c 4 5 5 5 b 4 b 5 5 4 c 
-        . c 5 c 4 c 5 5 5 c 4 c 5 5 5 c 
-        . c 5 c 4 c 5 5 5 5 c 5 5 5 5 c 
-        . c c c . . c c c c c c c c c . 
-        `],
-    100,
-    false
-    )
     if (theG.vy == 0) {
         theG.vy = -150
     }
 })
-controller.left.onEvent(ControllerButtonEvent.Pressed, function () {
-    animation.runImageAnimation(
-    theG,
-    [img`
-        . . . . . . . . . . . c c . . . 
-        . . . . . . . c c c c 6 3 c . . 
-        . . . . . . c 6 3 3 3 3 6 c . . 
-        . . c c . c 6 c c 3 3 3 3 3 c . 
-        . b 5 5 c 6 c 5 5 c 3 3 3 3 3 c 
-        . f f 5 c 6 c 5 f f 3 3 3 3 3 c 
-        . f f 5 c 6 c 5 f f 6 3 3 3 c c 
-        . b 5 5 3 c 3 5 5 c 6 6 6 6 c c 
-        . . b 5 5 3 5 5 c 3 3 3 3 3 3 c 
-        . . c 5 5 5 5 b c c 3 3 3 3 3 c 
-        . . c 4 5 5 4 b 5 5 c 3 3 3 c . 
-        . c 5 b 4 4 b b 5 c c b b b . . 
-        . c 4 4 b 5 5 5 4 c 4 4 4 5 b . 
-        . c 5 4 c 5 5 5 c 4 4 4 c 5 c . 
-        . c 5 c 5 5 5 5 c 4 4 4 c c c . 
-        . . c c c c c c c . . . . . . . 
-        `,img`
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . c c . . . . 
-        . . . . . . c c c c 6 3 c . . . 
-        . . . . . c 6 6 3 3 3 6 c . . . 
-        . . . . c 6 6 3 3 3 3 3 3 c . . 
-        b c c c 6 6 c c 3 3 3 3 3 3 c . 
-        b 5 5 c 6 c 5 5 c 3 3 3 3 3 c . 
-        f f 5 c 6 c 5 f f 6 3 3 3 c c . 
-        f f 5 c c c 5 f f 6 6 6 6 c c . 
-        . b 5 5 3 5 5 c 3 3 3 3 3 3 c . 
-        . c 5 5 5 5 4 c c c 3 3 3 3 c . 
-        . c 4 5 5 4 4 b 5 5 c 3 3 c . . 
-        . c 5 b 4 4 b b 5 c b b c . . . 
-        . c c 5 4 c 5 5 5 c c 5 c . . . 
-        . . . c c 5 5 5 5 c c c c . . . 
-        . . . . c c c c c c . . . . . . 
-        `,img`
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . c c . . . 
-        . . . . . . . c c c c 6 3 c . . 
-        . . . . . . c 6 6 3 3 3 6 c . . 
-        . . . . . c 6 6 3 3 3 3 3 3 c . 
-        . b c c c 6 6 c c 3 3 3 3 3 3 c 
-        . b 5 5 c 6 c 5 5 c 3 3 3 3 3 c 
-        . f f 5 c 6 c 5 f f 6 3 3 3 c c 
-        . f f 5 c c c 5 f f 6 6 6 6 c c 
-        . . b 5 5 3 5 5 c c c 3 3 3 3 c 
-        . . c 5 5 5 5 5 b 5 5 c 3 3 3 c 
-        . c 4 4 5 5 4 4 b b 5 c 3 3 c . 
-        . c 5 5 b 4 4 4 b 5 5 5 b c . . 
-        . c 5 5 5 4 4 4 c 5 5 5 c b . . 
-        . . c c c c 4 c 5 5 5 5 c c . . 
-        . . . . c c c c c c c c c c . . 
-        `,img`
-        . . . . . . . . . . . c c . . . 
-        . . . . . . . c c c c 6 3 c . . 
-        . . . . . . c 6 3 3 3 3 6 c . . 
-        . . c c . c 6 c c 3 3 3 3 3 c . 
-        . b 5 5 c 6 c 5 5 c 3 3 3 3 3 c 
-        . f f 5 c 6 c 5 f f 3 3 3 3 3 c 
-        . f f 5 c 6 c 5 f f 6 3 3 3 c c 
-        . b 5 5 3 c 3 5 5 c 6 6 6 6 c c 
-        . . b 5 5 3 5 5 c 3 3 3 3 3 3 c 
-        . c c 5 5 5 5 4 c c 3 3 3 3 3 c 
-        c 5 5 4 5 5 4 c 5 5 c 3 3 3 c . 
-        b 5 4 b 4 4 4 c 5 5 5 b c c . . 
-        c 4 5 5 b 4 4 c 5 5 5 c b b . . 
-        c 5 5 5 c 4 c 5 5 5 5 c c 5 b . 
-        c 5 5 5 5 c 4 c c c c c c 5 c . 
-        . c c c c c c . . . . . c c c . 
-        `],
-    100,
-    true
-    )
+function makeBacon () {
+    baconScore = sprites.create(img`
+        . . e e e e e . . . . . . . . . 
+        e e 2 2 2 2 2 e . . . . . . . . 
+        e 2 d d d d 2 2 e e e . . e e e 
+        e d 3 3 3 3 d 2 2 2 2 e e 2 2 e 
+        e 3 3 3 2 2 3 d d d 2 2 2 2 d e 
+        e 2 2 2 d d 2 2 2 3 d d d d 2 e 
+        e d d d 2 2 d d d 2 2 2 2 2 d e 
+        e 2 2 2 e 2 2 2 2 d d d d d 2 e 
+        e e e e . e e e 2 2 2 2 2 2 2 e 
+        . . . . . . . . e e e e e e e . 
+        `, SpriteKind.Food)
+    tiles.placeOnRandomTile(baconScore, assets.tile`myTile3`)
+}
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Food, function (sprite, otherSprite) {
+    info.changeScoreBy(1)
+    otherSprite.destroy()
+    music.baDing.play()
 })
-controller.right.onEvent(ControllerButtonEvent.Released, function () {
-    animation.runImageAnimation(
-    theG,
-    [img`
-        . . . c c . . . . . . . . . . . 
-        . . c 3 6 c c c c . . . . . . . 
-        . . c 6 3 3 3 3 6 c . . . . . . 
-        . c 3 3 3 3 3 c c 6 c . c c . . 
-        c 3 3 3 3 3 c 5 5 c 6 c 5 5 b . 
-        c 3 3 3 3 3 f f 5 c 6 c 5 f f . 
-        c c 3 3 3 6 f f 5 c 6 c 5 f f . 
-        c c 6 6 6 6 c 5 5 3 c 3 5 5 b . 
-        c 3 3 3 3 3 3 c 5 5 3 5 5 b . . 
-        c 3 3 3 3 3 c c b 5 5 5 5 c . . 
-        . c 3 3 3 c 5 5 b 4 5 5 4 c . . 
-        . . b b b c c 5 b b 4 4 b 5 c . 
-        . b 5 4 4 4 c 4 5 5 5 b 4 4 c . 
-        . c 5 c 4 4 4 c 5 5 5 c 4 5 c . 
-        . c c c 4 4 4 c 5 5 5 5 c 5 c . 
-        . . . . . . . c c c c c c c . . 
-        `],
-    100,
-    true
-    )
-})
-controller.left.onEvent(ControllerButtonEvent.Released, function () {
-    animation.runImageAnimation(
-    theG,
-    [img`
-        . . . . . . . . . . . c c . . . 
-        . . . . . . . c c c c 6 3 c . . 
-        . . . . . . c 6 3 3 3 3 6 c . . 
-        . . c c . c 6 c c 3 3 3 3 3 c . 
-        . b 5 5 c 6 c 5 5 c 3 3 3 3 3 c 
-        . f f 5 c 6 c 5 f f 3 3 3 3 3 c 
-        . f f 5 c 6 c 5 f f 6 3 3 3 c c 
-        . b 5 5 3 c 3 5 5 c 6 6 6 6 c c 
-        . . b 5 5 3 5 5 c 3 3 3 3 3 3 c 
-        . . c 5 5 5 5 b c c 3 3 3 3 3 c 
-        . . c 4 5 5 4 b 5 5 c 3 3 3 c . 
-        . c 5 b 4 4 b b 5 c c b b b . . 
-        . c 4 4 b 5 5 5 4 c 4 4 4 5 b . 
-        . c 5 4 c 5 5 5 c 4 4 4 c 5 c . 
-        . c 5 c 5 5 5 5 c 4 4 4 c c c . 
-        . . c c c c c c c . . . . . . . 
-        `],
-    100,
-    true
-    )
-})
-controller.right.onEvent(ControllerButtonEvent.Pressed, function () {
-    animation.runImageAnimation(
-    theG,
-    [img`
-        . . . c c . . . . . . . . . . . 
-        . . c 3 6 c c c c . . . . . . . 
-        . . c 6 3 3 3 3 6 c . . . . . . 
-        . c 3 3 3 3 3 c c 6 c . c c . . 
-        c 3 3 3 3 3 c 5 5 c 6 c 5 5 b . 
-        c 3 3 3 3 3 f f 5 c 6 c 5 f f . 
-        c c 3 3 3 6 f f 5 c 6 c 5 f f . 
-        c c 6 6 6 6 c 5 5 3 c 3 5 5 b . 
-        c 3 3 3 3 3 3 c 5 5 3 5 5 b . . 
-        c 3 3 3 3 3 c c b 5 5 5 5 c . . 
-        . c 3 3 3 c 5 5 b 4 5 5 4 c . . 
-        . . b b b c c 5 b b 4 4 b 5 c . 
-        . b 5 4 4 4 c 4 5 5 5 b 4 4 c . 
-        . c 5 c 4 4 4 c 5 5 5 c 4 5 c . 
-        . c c c 4 4 4 c 5 5 5 5 c 5 c . 
-        . . . . . . . c c c c c c c . . 
-        `,img`
-        . . . . . . . . . . . . . . . . 
-        . . . . c c . . . . . . . . . . 
-        . . . c 3 6 c c c c . . . . . . 
-        . . . c 6 3 3 3 6 6 c . . . . . 
-        . . c 3 3 3 3 3 3 6 6 c . . . . 
-        . c 3 3 3 3 3 3 c c 6 6 c c c b 
-        . c 3 3 3 3 3 c 5 5 c 6 c 5 5 b 
-        . c c 3 3 3 6 f f 5 c 6 c 5 f f 
-        . c c 6 6 6 6 f f 5 c c c 5 f f 
-        . c 3 3 3 3 3 3 c 5 5 3 5 5 b . 
-        . c 3 3 3 3 c c c 4 5 5 5 5 c . 
-        . . c 3 3 c 5 5 b 4 4 5 5 4 c . 
-        . . . c b b c 5 b b 4 4 b 5 c . 
-        . . . c 5 c c 5 5 5 c 4 5 c c . 
-        . . . c c c c 5 5 5 5 c c . . . 
-        . . . . . . c c c c c c . . . . 
-        `,img`
-        . . . . . . . . . . . . . . . . 
-        . . . c c . . . . . . . . . . . 
-        . . c 3 6 c c c c . . . . . . . 
-        . . c 6 3 3 3 6 6 c . . . . . . 
-        . c 3 3 3 3 3 3 6 6 c . . . . . 
-        c 3 3 3 3 3 3 c c 6 6 c c c b . 
-        c 3 3 3 3 3 c 5 5 c 6 c 5 5 b . 
-        c c 3 3 3 6 f f 5 c 6 c 5 f f . 
-        c c 6 6 6 6 f f 5 c c c 5 f f . 
-        c 3 3 3 3 c c c 5 5 3 5 5 b . . 
-        c 3 3 3 c 5 5 b 5 5 5 5 5 c . . 
-        . c 3 3 c 5 b b 4 4 5 5 4 4 c . 
-        . . c b 5 5 5 b 4 4 4 b 5 5 c . 
-        . . b c 5 5 5 c 4 4 4 5 5 5 c . 
-        . . c c 5 5 5 5 c 4 c c c c . . 
-        . . c c c c c c c c c c . . . . 
-        `,img`
-        . . . c c . . . . . . . . . . . 
-        . . c 3 6 c c c c . . . . . . . 
-        . . c 6 3 3 3 3 6 c . . . . . . 
-        . c 3 3 3 3 3 c c 6 c . c c . . 
-        c 3 3 3 3 3 c 5 5 c 6 c 5 5 b . 
-        c 3 3 3 3 3 f f 5 c 6 c 5 f f . 
-        c c 3 3 3 6 f f 5 c 6 c 5 f f . 
-        c c 6 6 6 6 c 5 5 3 c 3 5 5 b . 
-        c 3 3 3 3 3 3 c 5 5 3 5 5 b . . 
-        c 3 3 3 3 3 c c 4 5 5 5 5 c c . 
-        . c 3 3 3 c 5 5 c 4 5 5 4 5 5 c 
-        . . c c b 5 5 5 c 4 4 4 b 4 5 b 
-        . . b b c 5 5 5 c 4 4 b 5 5 4 c 
-        . b 5 c c 5 5 5 5 c 4 c 5 5 5 c 
-        . c 5 c c c c c c 4 c 5 5 5 5 c 
-        . c c c . . . . . c c c c c c . 
-        `],
-    100,
-    true
-    )
-})
-controller.down.onEvent(ControllerButtonEvent.Pressed, function () {
-    if (theG.vx == 0) {
-        animation.runImageAnimation(
-        theG,
-        [img`
-            . . . c c . . . . . . . . . . . 
-            . . c 3 6 c c c c . . . . . . . 
-            . . c 6 3 3 3 3 6 c . . . . . . 
-            . c 3 3 3 3 3 c c 6 c . c c . . 
-            c 3 3 3 3 3 c 5 5 c 6 c 5 5 b . 
-            c 3 3 3 3 3 f f 5 c 6 c 5 f f . 
-            c c 3 3 3 6 f f 5 c 6 c 5 f f . 
-            c c 6 6 6 6 c 5 5 3 c 3 5 5 b . 
-            c 3 3 3 3 3 3 c 5 5 3 5 5 b . . 
-            c 3 3 3 3 c c b 5 5 5 5 5 c c . 
-            . c 3 3 c 5 5 b 4 5 5 5 4 5 5 c 
-            . . b b c 5 b b 4 4 4 4 b 4 5 b 
-            . b 5 4 c 4 5 5 5 b 4 b 5 5 4 c 
-            . c 5 c 4 c 5 5 5 c 4 c 5 5 5 c 
-            . c 5 c 4 c 5 5 5 5 c 5 5 5 5 c 
-            . c c c . . c c c c c c c c c . 
-            `,img`
-            . . . . . . . . . . . . . . . . 
-            . . . . . . . . . . . . . . . . 
-            . . . c c . . . . . . . . . . . 
-            . . c 3 6 c c c c . . . . . . . 
-            . . c 6 3 3 3 3 6 c . . . . . . 
-            . c 3 3 3 3 3 c c 6 c . c c . . 
-            c 3 3 3 3 3 c 5 5 c 6 c 5 5 c . 
-            c c 3 3 3 6 f f 5 c 6 c 5 f f . 
-            c c 6 6 6 6 f f 5 3 c 3 5 f f . 
-            c 3 3 3 3 3 3 c 5 5 3 5 5 b . . 
-            c 3 3 3 3 c c b 5 5 5 5 5 c c . 
-            . c 3 3 c 5 5 b 4 5 5 5 4 5 5 c 
-            . . b b c 5 b b 4 4 4 4 b 4 5 b 
-            . c 5 c 4 c 5 5 5 c 4 c 5 5 5 c 
-            . c 5 c 4 c 5 5 5 5 c 5 5 5 5 c 
-            . c c c . . c c c c c c c c c . 
-            `,img`
-            . . . c c . . . . . . . . . . . 
-            . . c 3 6 c 3 c c c c . c c . . 
-            . . c 6 3 3 f f 5 c 6 c 5 f f . 
-            . c 3 3 3 3 f f 5 c 6 c 5 f f . 
-            c 3 3 3 3 3 c 5 5 5 c 5 5 5 b . 
-            c 3 3 3 3 6 c 5 5 5 c 5 5 5 b . 
-            c c 3 3 3 6 3 c 5 5 3 5 5 b b . 
-            c c 6 6 3 3 3 c 5 5 3 5 5 b b . 
-            c 3 3 3 3 c c b 5 5 5 5 5 c c . 
-            c 3 3 3 c 5 5 b 4 5 5 5 4 5 5 c 
-            . c 3 3 c 5 b b 4 4 4 4 b 4 5 b 
-            . . b b c 4 5 5 5 b 4 b 5 5 4 c 
-            . b 5 4 4 c 5 5 5 c 4 c 5 5 5 c 
-            . c 5 c 4 c 5 5 5 c 4 c 5 5 5 c 
-            . c 5 c 4 c 5 5 5 5 c 5 5 5 5 c 
-            . c c c . . c c c c c c c c c . 
-            `,img`
-            . . . . . . . . . . . . . . . . 
-            . . . c c . . . . . . . . . . . 
-            . . c 3 6 c c c c . . . . . . . 
-            . . c 6 3 3 3 3 6 c . . . . . . 
-            . c 3 3 3 3 3 3 6 6 c . . . . . 
-            c 3 3 3 3 3 3 6 6 6 6 c . . . . 
-            c 3 3 3 3 3 c c 6 6 c c c c c . 
-            c c 3 3 3 c 5 5 c c 3 3 c 5 5 b 
-            c c 6 6 6 f f 5 c 3 c c c 5 f f 
-            c 3 3 3 3 f f 5 c c c c c 5 f f 
-            c 3 3 3 3 3 c 5 5 3 c 3 5 5 b . 
-            . c 3 3 3 c c 4 5 5 5 5 4 4 c . 
-            . . b b c 4 5 5 4 4 4 4 5 5 4 c 
-            . c 5 c 4 c 5 5 5 c 4 c 5 5 5 c 
-            . c 5 c 4 c 5 5 5 5 c 5 5 5 5 c 
-            . c c c . . c c c c c c c c c . 
-            `,img`
-            . . . . . . . . . . . . . . . . 
-            . . . . . . . . . . . . . . . . 
-            . . . . . . . . . . . . . . . . 
-            . . . . . . . . . . . . . . . . 
-            . . . c c . . . . . . . . . . . 
-            . . c 3 6 c c c c c . . . . . . 
-            . c c 6 3 3 3 3 6 6 c . . . . . 
-            c 6 3 3 3 3 3 6 6 6 6 c . . . . 
-            c 3 3 3 3 3 6 6 6 6 6 c . . . . 
-            c 3 3 3 3 6 6 6 6 6 6 c c c . . 
-            c c 6 6 6 6 6 6 6 6 c 3 3 3 c . 
-            c 6 3 3 3 6 6 6 c c 3 c c c 6 c 
-            c 3 3 3 3 3 6 c c c c c c c c c 
-            . c c c 6 6 c 4 5 5 c c 4 5 5 c 
-            . . . c 6 6 6 c 5 5 5 c 5 5 4 c 
-            . . . . c c c c c c c c c c c . 
-            `,img`
-            . . . . . . . . . . . . . . . . 
-            . . . . . . . . . . . . . . . . 
-            . . . . . . . . . . . . . . . . 
-            . . c c . . . . . . . . . . . . 
-            . c 3 6 c c c c c . . . . . . . 
-            . c 6 6 3 3 3 6 6 c . . . . . . 
-            c 3 3 3 3 3 3 6 6 6 c . . . . . 
-            c 3 3 3 3 3 3 6 6 6 6 c . . . . 
-            c 3 3 3 3 3 6 6 6 6 6 c c c . . 
-            c 3 3 3 3 6 6 6 6 6 c 3 3 3 c . 
-            c c 3 3 6 6 6 6 6 c 3 c c c 3 c 
-            c c 6 6 6 6 6 6 c 3 c c c c 6 c 
-            c 3 3 3 3 6 6 c 6 6 c c c c 6 c 
-            c 6 3 3 3 3 6 c 6 c c c c 6 c . 
-            . c 6 3 3 3 6 c c c c c 6 c . . 
-            . . c c c c c c c c c c c . . . 
-            `],
-        100,
-        false
-        )
-    }
-    while (true) {
-        theG.vx = 0
-    }
-})
+let baconScore: Sprite = null
 let theG: Sprite = null
-tiles.setCurrentTilemap(tilemap`level1`)
+tiles.setCurrentTilemap(tilemap`level2`)
 theG = sprites.create(img`
-    . . . c c . . . . . . . . . . . 
-    . . c 3 6 c c c c . . . . . . . 
-    . . c 6 3 3 3 3 6 c . . . . . . 
-    . c 3 3 3 3 3 c c 6 c . c c . . 
-    c 3 3 3 3 3 c 5 5 c 6 c 5 5 b . 
-    c 3 3 3 3 3 f f 5 c 6 c 5 f f . 
-    c c 3 3 3 6 f f 5 c 6 c 5 f f . 
-    c c 6 6 6 6 c 5 5 3 c 3 5 5 b . 
-    c 3 3 3 3 3 3 c 5 5 3 5 5 b . . 
-    c 3 3 3 3 c c b 5 5 5 5 5 c c . 
-    . c 3 3 c 5 5 b 4 5 5 5 4 5 5 c 
-    . . b b c 5 b b 4 4 4 4 b 4 5 b 
-    . b 5 4 c 4 5 5 5 b 4 b 5 5 4 c 
-    . c 5 c 4 c 5 5 5 c 4 c 5 5 5 c 
-    . c 5 c 4 c 5 5 5 5 c 5 5 5 5 c 
-    . c c c . . c c c c c c c c c . 
+    ....dd............dd....
+    .eddd333........333dddd.
+    eeedccc333....333cccdddd
+    .dddcbbcddddddddcbbcddd.
+    .ddcbbddddddddddddbbcdd.
+    ..3bbddddddddeeedddbb3..
+    ..333dd1ddddeeee1dd333..
+    ....dddfdddddeedfddd....
+    ....dddfdddddeedfddd....
+    ...ddeddd333333ddddee...
+    ...eeeed33c33c33ddeee...
+    ...eeeed33c33c3eddeee...
+    ...eeeed33c33ceeddeee...
+    ...beeddd33333edddeeb...
+    ....bbddddddddddddbb....
+    ......bbbbbbbbbbbb......
     `, SpriteKind.Player)
+theG.setPosition(0, 200)
 controller.moveSprite(theG, 100, 0)
-theG.ay = 300
-scene.cameraFollowSprite(theG)
 theG.setStayInScreen(true)
+scene.cameraFollowSprite(theG)
+theG.ay = 200
+info.setScore(0)
 forever(function () {
-	
+    for (let index = 0; index < 50; index++) {
+        makeBacon()
+    }
 })
